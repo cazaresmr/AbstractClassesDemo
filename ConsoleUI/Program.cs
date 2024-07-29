1c2acbc15b788bb58428225092625cf64b278996
Michael Cazares <michaelcazares@gmail.com>
2024-07-28 21:14:08 -0700

    completed exercise

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -1,38 +1,52 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ConsoleUI
 {
+    public abstract class Vehicle
+    {
+        public string Year { get; set; } = "Unknown Year";
+        public string Make { get; set; } = "Unknown Make";
+        public string Model { get; set; } = "Unknown Model";
+
+        public abstract void DriveAbstract();
+        public virtual void DriveVirtual()
+        {
+            Console.WriteLine("This is a generic vehicle driving.");
+        }
+    }
+
+    public class Car : Vehicle
+    {
+        public bool HasTrunk { get; set; }
+
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("The car is driving.");
+        }
+    }
+
+    public class Motorcycle : Vehicle
+    {
+        public bool HasSideCart { get; set; }
+
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("The motorcycle is driving.");
+        }
+
+        public override void DriveVirtual()
+        {
+            Console.WriteLine("This motorcycle is driving differently.");
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            /*
-             * Todo: Follow all comments!! 
-             * Double click on the region (gray box) to view all comments
-             */
-
-            #region Vehicles
-
-            /*
-             * Create an abstract class called Vehicle
-             * The vehicle class shall have three string properties: Year, Make, and Model
-             * Set the defaults of the properties to something generic in the Vehicle class
-             * Vehicle class shall have an abstract method called DriveAbstract with no implementation.
-             * Vehicle class shall have a virtual method called DriveVirtual with a base implementation.
-             */
-
-            /* 
-             * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
-             * Add a distinct property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
-             * Provide the implementations for the abstract methods
-             * Only in the Motorcycle class will you override the virtual drive method
-            */
-
             // Create a list of Vehicle called vehicles
+            List<Vehicle> vehicles = new List<Vehicle>();
 
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
@@ -40,14 +54,32 @@ namespace ConsoleUI
              * Set the properties values with object initializer syntax
              */
 
+            var car1 = new Car { Year = "2021", Make = "Toyota", Model = "Camry", HasTrunk = true };
+            var motorcycle1 = new Motorcycle { Year = "2022", Make = "Harley-Davidson", Model = "Sportster", HasSideCart = false };
+            Vehicle vehicle1 = new Car { Year = "2019", Make = "Honda", Model = "Civic", HasTrunk = false };
+            Vehicle vehicle2 = new Motorcycle { Year = "2020", Make = "Ducati", Model = "Monster", HasSideCart = true };
+
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate through the list and display each of the properties
              */
+            vehicles.Add(car1);
+            vehicles.Add(motorcycle1);
+            vehicles.Add(vehicle1);
+            vehicles.Add(vehicle2);
+
+            foreach (var vehicle in vehicles)
+            {
+                Console.WriteLine($"Year: {vehicle.Year}, Make: {vehicle.Make}, Model: {vehicle.Model}");
+            }
 
             // Call each of the drive methods for one car and one motorcycle
+            car1.DriveAbstract();
+            car1.DriveVirtual();
 
-            #endregion            
+            motorcycle1.DriveAbstract();
+            motorcycle1.DriveVirtual();
+
             Console.ReadLine();
         }
     }
